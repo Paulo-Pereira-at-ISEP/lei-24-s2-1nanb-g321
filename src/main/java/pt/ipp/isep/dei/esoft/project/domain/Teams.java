a58830e691b30133bb394da84bc56b76d16be76a
Alexandre_Guerra <1220961@isep.ipp.pt>
2024-04-24 23:10:35 +0100

    LGGPM-135 - Update Teams Class

--- src/main/java/pt/ipp/isep/dei/esoft/project/domain/Teams.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/domain/Teams.java
@@ -3,12 +3,18 @@ package pt.ipp.isep.dei.esoft.project.domain;
 import pt.ipp.isep.dei.esoft.project.repository.SkillRepository;
 
 public class Teams {
+    private String teamMembers;
     private int teamSize;
     public SkillRepository listOfSkills;
 
-    public Teams(int teamSize, SkillRepository listOfSkills) {
+    public Teams(String teamMembers, int teamSize, SkillRepository listOfSkills) {
+        this.teamMembers = teamMembers;
         this.teamSize = teamSize;
         this.listOfSkills = listOfSkills;
+    }
+
+    public String toString(){
+        return teamMembers + "\t" + teamSize + "\t" + listOfSkills;
     }
 
     public int getTeamSize() {
