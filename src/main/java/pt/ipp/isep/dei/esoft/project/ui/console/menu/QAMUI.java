82a40c218f97b4894bcf2bfe642c108dcee8145a
Paulo Pereira <1221967@isep.ipp.pt>
2024-05-12 15:42:23 +0100

    added menu options for US 13 and 14

--- src/main/java/pt/ipp/isep/dei/esoft/project/ui/console/menu/QAMUI.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/ui/console/menu/QAMUI.java
@@ -3,6 +3,7 @@ package pt.ipp.isep.dei.esoft.project.ui.console.menu;
 
 import pt.ipp.isep.dei.esoft.project.ui.console.CreateJobUI;
 import pt.ipp.isep.dei.esoft.project.ui.console.CreateSkillUI;
+import pt.ipp.isep.dei.esoft.project.ui.console.ExecutionTimeAnalysis;
 import pt.ipp.isep.dei.esoft.project.ui.console.ShowTextUI;
 import pt.ipp.isep.dei.esoft.project.ui.console.utils.Utils;
 
@@ -19,11 +20,8 @@ public class QAMUI implements Runnable {
 
     public void run() {
         List<MenuItem> options = new ArrayList<MenuItem>();
-        options.add(new MenuItem("Create Skill", new CreateSkillUI()));
-        options.add(new MenuItem("Create Job", new CreateJobUI()));
-        options.add(new MenuItem("Register Collaborator", new ShowTextUI("You have chosen Option 3.")));
-        options.add(new MenuItem("Assign one or more skills to a collaborator", new ShowTextUI("You have chosen Option 4.")));
-        options.add(new MenuItem("Generate a team proposal", new ShowTextUI("You have chosen Option 3.")));
+        options.add(new MenuItem("Algorithm Efficiency Analysis", new ExecutionTimeAnalysis()));
+
 
         int option = 0;
         do {
