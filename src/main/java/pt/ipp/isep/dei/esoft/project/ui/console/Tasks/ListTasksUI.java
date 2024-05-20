23d56dfce5dd5cf255122c4e5f3b25c96bb34e5a
Paulojfs <1120629@isep.ipp.pt>
2024-05-20 20:58:20 +0100

    LGGPM - 143 Create ListTasksUI

---
+++ src/main/java/pt/ipp/isep/dei/esoft/project/ui/console/Tasks/ListTasksUI.java
@@ -0,0 +1,45 @@
+package pt.ipp.isep.dei.esoft.project.ui.console.Tasks;
+
+import pt.ipp.isep.dei.esoft.project.application.controller.CreateSkillController;
+
+import pt.ipp.isep.dei.esoft.project.application.controller.CreateTaskController;
+import pt.ipp.isep.dei.esoft.project.domain.Skill;
+import pt.ipp.isep.dei.esoft.project.domain.Task;
+import pt.ipp.isep.dei.esoft.project.ui.console.Tasks.TaskUI;
+
+import java.util.List;
+
+/**
+ * List Skill UI (console). This option is only available for administrators for demonstration purposes.
+ */
+public class ListTasksUI implements Runnable {
+
+    private final CreateTaskController taskController;
+
+    public ListTasksUI() {
+        taskController = new CreateTaskController();
+    }
+
+    public void run() {
+        System.out.println("\n\n--- List Tasks ------------------------");
+        listTasks();
+    }
+
+    /**
+     * Lists all registered skills along with their details.
+     */
+    private void listTasks() {
+        List<Task> tasks = taskController.getAllTasks();
+
+        System.out.println("Registered Tasks:");
+        int counter = 1;
+        for (Task task : tasks) {
+
+            System.out.println("[" + counter + "]   Title: " + task.getName());
+            System.out.println("      Description: " + task.getDescription());
+            System.out.println("-------------------------");
+            counter++;
+        }
+    }
+
+}
