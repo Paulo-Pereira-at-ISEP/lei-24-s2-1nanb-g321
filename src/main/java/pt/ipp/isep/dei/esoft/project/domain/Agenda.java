819672af68b185b412deea5b0311d8738ec0a40c
PaulojfSilva <1120629@isep.ipp.pt>
2024-05-27 18:26:51 +0100

    Update agenda and ToDoList classes.

--- src/main/java/pt/ipp/isep/dei/esoft/project/domain/Agenda.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/domain/Agenda.java
@@ -1,19 +1,4 @@
 package pt.ipp.isep.dei.esoft.project.domain;
 
-public class Agenda extends Planning  {
-private String date;
-
-public Agenda(Task task, double expectedDuration, String urgencyDegree, String date) {
-    super(task, expectedDuration, urgencyDegree);
-    this.date = date;
-}
-
-public String getDate() {
-    return date;
-}
-
-@Override
-public boolean changeStatus() {
-    return !status;
-}
+public class Agenda {
 }
--- src/main/java/pt/ipp/isep/dei/esoft/project/domain/ToDoList.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/domain/ToDoList.java
@@ -1,18 +1,5 @@
 package pt.ipp.isep.dei.esoft.project.domain;
 
-public class ToDoList extends Planning {
+public class ToDoList {
 
-    public ToDoList(Task task, double expectedDuration, String urgencyDegree) {
-        super(task, expectedDuration, urgencyDegree); // Call superclass constructor
-    }
-
-    @Override
-    public boolean changeStatus() {
-
-        return status;
-    }
-    public ToDoList clone() {
-        return new ToDoList(this.task, this.expectedDuration, this.urgencyDegree);
-    }
 }
-
